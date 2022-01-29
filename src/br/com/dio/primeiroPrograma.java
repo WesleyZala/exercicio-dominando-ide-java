278104ff49c7eac8c4c685811104b4aec476dc93
WesleyZala <wesley.zala.rodrigues@gmail.com>
2022-01-28 23:02:50 -0300

    primeiro commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import  br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+//        System.out.printf("Hello World");
+    }
+}
